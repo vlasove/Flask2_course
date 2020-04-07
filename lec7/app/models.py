651bcd28e85e3395d5f9f1c5b687d5fb0d2cc2ca
vlasove <evgeny_vlasov@yahoo.com>
2020-04-07 10:55:11 +0300

    added full info about posts model

--- lec7/app/models.py
+++ lec7/app/models.py
@@ -1,13 +1,22 @@
-from app import db 
+from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50), index=True, unique=True)
     password = db.Column(db.String(60))
     email = db.Column(db.String(100), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format( self.username)
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True) 
+    id = db.Column(db.Integer, primary_key = True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    body = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
